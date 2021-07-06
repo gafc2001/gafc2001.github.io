11e23f28701be86b52bd112dcf48104bd0ab8485
Gustavo Farfan <farfancoraje15@gmail.com>
2021-07-06 14:48:53 -0500

    Fixing 404 problems

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,4 @@ let barProgress = document.getElementsByClassName('progress');
 for (const element of barProgress) {
     progress = element.getAttribute('data-lvl');
     element.style.width = progress + "%";
-    console.log(element);
 }
