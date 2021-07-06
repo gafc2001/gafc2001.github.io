c41a642cff9b463547a90b38e8875dce361b2794
Gustavo Farfan <farfancoraje15@gmail.com>
2021-07-06 14:10:35 -0500

    skills and footer styles

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+let barProgress = document.getElementsByClassName('progress');
+for (const element of barProgress) {
+    progress = element.getAttribute('data-lvl');
+    element.style.width = progress + "%";
+    console.log(element);
+}
