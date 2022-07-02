3dfa174549f9787ba3ee15d04dded8fb97ad7254
Gustavo Farfan <farfancoraje15@gmail.com>
2022-07-02 18:35:51 -0500

    test env production

--- src/services/Visit/index.js
+++ src/services/Visit/index.js
@@ -1,10 +1,15 @@
 
 const increaseVisit = async () => {
     const api = `${process.env.REACT_APP_API_URL}/api/increase`;
-    const resp = await fetch(api,{
-        'method' : 'POST',
-    })
-    console.log(resp.text());
+    if(!localStorage.getItem('visitor')){
+        const resp = await fetch(api,{
+            'method' : 'POST',
+        }).then(resp => resp.json())
+        .then(json => json.data)
+        localStorage.setItem('visitor',JSON.stringify(resp));
+        console.log('n');
+    }
+    
 }
 
 export {
