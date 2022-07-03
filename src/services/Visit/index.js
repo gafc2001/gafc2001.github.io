7098dfc625d7b66191cc5c1c29625adef28f4339
Gustavo Farfan <farfancoraje15@gmail.com>
2022-07-02 21:18:46 -0500

    use ipinfo

--- src/services/Visit/index.js
+++ src/services/Visit/index.js
@@ -1,12 +1,35 @@
 
 const increaseVisit = async () => {
     const api = `${process.env.REACT_APP_API_URL}/api/increase`;
+    const token_ipinfo = 'd11e824e6cfad7';
     if(!localStorage.getItem('visitor')){
-        const resp = await fetch(api,{
+
+        const request = await fetch(`https://ipinfo.io/json?token=${token_ipinfo}`)
+                            .then(resp => resp.json())
+                            .then(info => {
+                                return {
+                                    'ip_address':info.ip,
+                                    'country':info.country,
+                                    'city':info.city
+                                }
+                            })
+                            .catch(err => {
+                                console.log("Error : "+err);
+                                return {
+                                    'ip_address' : '0.0.0.0',
+                                    'country' : 'anonymous',
+                                    'city' : 'anonymous',
+                                }
+                            });
+        await fetch(api,{
             'method' : 'POST',
+            'body' : JSON.stringify(request),
         }).then(resp => resp.json())
-        .then(json => json.data)
-        localStorage.setItem('visitor',JSON.stringify(resp));
+        .then(json => {
+            localStorage.setItem('visitor',JSON.stringify(json.data));
+        })
+        .catch(err => console.log("Error : "+err));
+
         console.log('n');
     }
     
