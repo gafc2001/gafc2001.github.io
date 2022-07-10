f09a920c2d9a051b50286670036af80eaaedc0d9
Gustavo Farfan <farfancoraje15@gmail.com>
2022-07-09 21:37:45 -0500

    login design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,67 @@
-import React from 'react'
-
+import React,{useState,useMemo} from "react";
+import "./../assets/css/admin.css";
+import { Form, InputGroup, Button } from "react-bootstrap";
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+
+    
+    const initialState = useMemo(() => {
+        return {
+            "username" : "",
+            "password" : "",
+        }
+    })
+
+    const [validated,setValidated] = useState(false);
+    const [data, setData]  = useState(initialState);
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        setData({
+            ...data,
+            [e.target.name] : value,
+        })    
+        console.log(data)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data);
+    }
+    
+
+    return (
+    <main className="w-screen bg-gradient-login center">
+        <div className="login-container">
+            <Form className="bg-light p-5 rounded" 
+                 validated={validated}
+                 onSubmit={(e) => handleSubmit(e)}>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                    <i className="fas fa-user-secret"></i>
+                    </InputGroup.Text>
+                    <Form.Control
+                    name="username"
+                    placeholder="Username"
+                    aria-label="Username"
+                    required
+                    onChange={(e) => handleInputChange(e)}
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                    <i className="fas fa-lock"></i>
+                    </InputGroup.Text>
+                    <Form.Control
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    aria-label="Password"
+                    required
+                    onChange={(e) => handleInputChange(e)}
+                    />
+                </InputGroup>
+                <Button type="submit">Submit</Button>
+            </Form>
+        </div>
+    </main>
+  );
+};
