ea0128875633bc5a9c80fe55ec3a9ecb75a29a88
Gustavo Farfan <farfancoraje15@gmail.com>
2022-07-17 13:37:03 -0500

    Hashrouter

--- src/components/Admin/Main/SummaryBox.jsx
+++ src/components/Admin/Main/SummaryBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Col,Row} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
 import { Spinner } from 'react-bootstrap'
 export const SummaryBox = ({bgColor,isLoading,data}) => {
   return (
